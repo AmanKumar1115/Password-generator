4451d62efcb26737c4c0204bc5d35eb70774cc16
Aman <amankr5548@gmail.com>
2024-02-08 00:07:25 +0530

    Error correction  (useState to useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,72 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 
 
 function App() {
-  const [length,setLength] = useState(8);
-  const [numAllowed,setNumAllowed] = useState(false);
-  const [charAllowed,setCharAllowed] = useState(false);
-  const [password,setPassword] = useState("");
-  const passwordRef = useState(null);  
-  
-  const passwordGenerator =  useCallback(()=>{
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useRef is a React Hook that lets you reference a value that’s not needed for rendering.
+  const passwordRef = useRef(null);
+
+  //useCallback is a React Hook that lets you cache a function definition between re-renders.
+  const passwordGenerator = useCallback(() => {
     let pass = ""
-    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if(numAllowed){
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numAllowed) {
       str += "1234567890"
     }
-    if(charAllowed){
+    if (charAllowed) {
       str += "{[(`~!@#$%^&*_+-=)]}"
     }
 
-    for(let i=1;i<length;i++){
-      let char = Math.floor(Math.random()*str.length+1)
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
       pass += str.charAt(char);
     }
     setPassword(pass)
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[length, numAllowed, charAllowed, setPassword])
+  }, [length, numAllowed, charAllowed, setPassword])
 
-  const copyPassword = useCallback(()=>{
+  const copyPassword = useCallback(() => {
     passwordRef.current?.select()
     //passwordRef.current?.setSelectionRange(0,5);
     window.navigator.clipboard.writeText(password);
-  },[password, passwordRef])
+  }, [password, passwordRef])
 
-  useEffect(()=>{
+  //useEffect is a React Hook that lets you synchronize a component with an external system.
+  useEffect(() => {
     passwordGenerator();
-  },[length,numAllowed,charAllowed,passwordGenerator])
+  }, [length, numAllowed, charAllowed, passwordGenerator])
   return (
     <div className='w-full max-w-lg mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
       <h1 className='text-4xl text-center text-white my-3' >Password Generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-      <input
-        type='text'
-        value={password}
-        className='outline-none w-full py-1 px-3'
-        placeholder='Password'
-        readOnly
-        ref={passwordRef}
-      />
-      <button
-        onClick={copyPassword} 
-        type="button" 
-        className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center mr-2 rounded-r-lg dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">copy</button>
+        <input
+          type='text'
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='Password'
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          onClick={copyPassword}
+          type="button"
+          className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center mr-2 rounded-r-lg dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">copy</button>
       </div>
-      
+
       <div className='flex text-sm gap-x-2'>
         <div className='flex items-center gap-x-1'>
-          <input 
+          <input
             type="range"
             min={8}
             max={100}
-            value={length} 
+            value={length}
             className='cursor-pointer'
-            onChange={(event)=>{setLength(event.target.value)} }
+            onChange={(event) => { setLength(event.target.value) }}
           />
           <label>Lenght ={length}</label>
         </div>
@@ -72,26 +75,26 @@ function App() {
             type='checkbox'
             defaultChecked={numAllowed}
             id="numberInput"
-            onChange={()=>{
-              setNumAllowed((prev)=>!prev)
+            onChange={() => {
+              setNumAllowed((prev) => !prev)
             }}
           />
           <label htmlFor='numberInput'>Numbers</label>
         </div>
         <div className='flex items-center gap-x-1'>
-            <input
-              type='checkbox'
-              defaultChecked={charAllowed}
-              id="characterInput"
-              onChange={()=>{
-                setCharAllowed((prev)=>!prev)
-              }}
-            />
-            <label htmlFor='characterInput'>Special Characters</label>
+          <input
+            type='checkbox'
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev)
+            }}
+          />
+          <label htmlFor='characterInput'>Special Characters</label>
         </div>
       </div>
 
-    </div>  
+    </div>
   )
 }
 
